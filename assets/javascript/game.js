c93817f5dc9fc8434d01f99641f1a26e0744a19a
Alexandria Szoeke <alex_szoeke@outlook.com>
2018-02-13 23:33:25 -0800

    added random imagesgit status

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,10 @@
 //These are my variables
 //User starts off with 0 wins
 var wins = 0;
+var winMessage = ["You can't teach that!", "Let's see what other moves you got!", "Wow!", "Show off your winning moves for the dance cam"];
 //User starts off with 0 losses
 var losses = 0;
+var lossMessage = ["Where's the foul?!", "Fouled out!", "That's a technical", "Do you want the other team to win?!"];
 //the user has 12 attempts to guess the correct letters
 var guessesLeft = 12;
 var wrongLetters = [];
@@ -18,20 +20,21 @@ var newWord = "";
 var answers = [];
 //Array of words to guess
 var words = ["guard", "forward", "center", "basketball", "foul","referee", "rockets", "clutch", "fans", "rowdies"];
-var imgNumber = "";
-console.log(imgNumber);
+//Photo Variables
+var images = ["./assets/images/img_1.jpg", "./assets/images/img_2.jpg", "./assets/images/img_3.jpg", "./assets/images/img_4.jpg", "./assets/images/img_5.jpg", "./assets/images/img_6.jpg", "./assets/images/img_7.jpg", "./assets/images/img_8.jpg", "./assets/images/img_9.jpg", "./assets/images/img_10.jpg"];
+var imgURL = "";
+console.log(imgURL);
+
 
 //Functions
 function startGame() {
 
 //computer needs to randomly generate a new word
     newWord = words[Math.floor(Math.random() * words.length)];
-console.log(newWord);
 //correct letters
     correctLetters = newWord.split("");
 //the word needs to appear blank
     blanks = correctLetters.length;
-console.log(correctLetters);
 
 //Reset
 guessesLeft = 12;
@@ -44,6 +47,7 @@ for (var i=0; i<blanks; i++) {
 
 console.log(newWord);
 console.log(correctLetters);
+
 console.log(blanks);
 console.log(answers);
 
@@ -54,14 +58,17 @@ document.getElementById("wrong").innerHTML = "You've already guessed: "
 document.getElementById("wincount").innerHTML = wins;
 document.getElementById("losscount").innerHTML = losses;
 
+randomImg();
 }
-//Everything here to check letter function is my attempt to generate a new photo every time a new word is generated.
+
+//Everything here to the check letter function is my attempt to generate a new photo every time a new word is generated.
 function randomImg() {
-    var index = words.indexOf(newWord);
-    imgNumber = ("img_" + index);
-    document.getElementById("imgid").src = "../images/" + imgNumber;
-    console.log(index);
+    imgURL = images[Math.floor(Math.random() * images.length)];
+    console.log(imgURL);
 }
+
+
+
 
 //computer needs to recognize when letters are correct
 function checkLetter(letter) {
@@ -92,47 +99,51 @@ function checkLetter(letter) {
     console.log(guessesLeft);
 }
 
-
-
-
 function completed() {
     console.log("Win Count: " + wins + " | Loss Count: " + losses + " | Guesses Left: " + guessesLeft);
 
 
 document.getElementById("newword").innerHTML = answers.join(" ");
 document.getElementById("numGuesses").innerHTML = guessesLeft;
-document.getElementById("wrong").innerHTML = "You've already guessed: " + wrongLetters.join(" ");   
-document.getElementById("imgid").src = url("../images/" + imgNumber);
+document.getElementById("wrong").innerHTML = "You've already guessed: " + wrongLetters.join(" ");  
 
 //check if user wins or loses
 //once the word has been completed or the number of guesses has reached 0 a new word will be generated.
     if (correctLetters.toString() == answers.toString()) {
         wins++;
         setTimeout(function () {
-            alert("Clutch would be proud! You can't teach that!");
+            alert("You won! " + winMessage[Math.floor(Math.random() * winMessage.length)]);
         }, 200);
         document.getElementById("wincount").innerHTML = "Number of wins: " + wins;
         window.setTimeout(startGame, 400);
         
+        setTimeout(function () {
+        document.getElementById("imgid").src = imgURL;
+        }, 200);
+
+        
     } else if (guessesLeft == 0) {
         losses++;
         setTimeout(function () {
-        alert("Where's the foul?! You lost!");
+        alert(lossMessage[Math.floor(Math.random() * lossMessage.length)] + " The correct word was " + newWord);
         }, 200);
         document.getElementById("losscount").innerHTML = losses;
-        window.setTimeout(startGame, 400);
+        window.setTimeout(startGame, 400); 
+        setTimeout(function () {
+        document.getElementById("imgid").src = imgURL; 
+        }, 200);
+
+        }
+
     }
-}
 
     
-
+//Functions called here
 startGame();
 //users guess
 document.onkeyup = function (event) {
 var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 checkLetter(userGuess);
 completed();
-randomImg();
-
 console.log(userGuess);
 }
